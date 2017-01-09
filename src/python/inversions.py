0f7df219874a8af791d66300de93ed755c66a4bb
nglelinh <nglelinh@gmail.com>
2017-01-09 08:56:27 +0700

    sort

---
+++ src/python/inversions.py
@@ -0,0 +1,57 @@
+# sort the array using mergesort, meanwhile count the number of inversions
+
+input_numbers = []
+
+with open('/home/nglelinh/Downloads/IntegerArray.txt') as f:
+    for line in f:
+        input_numbers.append(int(line))
+
+
+def sort_and_count_inversions(numbers):
+    if len(numbers) < 2:
+        return numbers, 0
+
+    left, left_count = sort_and_count_inversions(numbers[:int(len(numbers) / 2)])
+    right, right_count = sort_and_count_inversions(numbers[int(len(numbers) / 2):])
+    merged, split_count = merge_split(left, right)
+
+    return merged, left_count + right_count + split_count
+
+
+def merge_split(left, right):
+    left_i = right_i = inversions = 0
+    sorted_list = []
+
+    for i in range(len(left) + len(right)):
+        if left_i == len(left):
+            sorted_list.append(right[right_i])
+            right_i += 1
+            continue
+
+        if right_i == len(right):
+            sorted_list.append(left[left_i])
+            left_i += 1
+            continue
+
+        if left[left_i] < right[right_i]:
+            sorted_list.append(left[left_i])
+            left_i += 1
+        else:
+            sorted_list.append(right[right_i])
+            right_i += 1
+            inversions += len(left) - left_i
+
+    return sorted_list, inversions
+
+
+merged_list, count = merge_split([2, 3, 5, 8], [1, 4, 6])
+
+print(merged_list)
+
+print(count)
+
+merged_list, count = sort_and_count_inversions(input_numbers)
+
+print(input_numbers[:10])
+
+print(count)
---
+++ src/python/quicksort.py
@@ -0,0 +1,77 @@
+import statistics
+
+# 1: first element, 2: last element, 3: median-of-three
+pivot_settings = 3
+inputs = []
+
+with open('/home/nglelinh/Downloads/QuickSort.txt') as f:
+    for line in f:
+        inputs.append(int(line))
+
+
+def get_index_of_median(numbers):
+    a, b, c = numbers[0], numbers[int((len(numbers) - 1) / 2)], numbers[-1]
+    median = statistics.median([a, b, c])
+    if b == median:
+        return int((len(numbers) - 1) / 2)
+    if a == median:
+        return 0
+    if c == median:
+        return len(numbers) - 1
+
+
+def choose_pivot_element(numbers):
+    if pivot_settings == 1:
+        return numbers[0], numbers[1:]
+    if pivot_settings == 2:
+        return numbers[-1], numbers[:-1]
+    if pivot_settings == 3:
+        index = get_index_of_median(numbers)
+        pivot = numbers[index]
+        del numbers[index]
+        return pivot, numbers
+
+
+def sort_and_count_comparisons(numbers):
+    if len(numbers) < 2:
+        return numbers, 0
+
+    left, pivot, right = subroutine(numbers)
+    left, left_count = sort_and_count_comparisons(left)
+    right, right_count = sort_and_count_comparisons(right)
+
+    return left + [pivot] + right, left_count + right_count + len(numbers) - 1
+
+
+def subroutine(numbers):
+    pivot, sub_array = choose_pivot_element(numbers)
+    left = []
+    right = []
+    for numb in sub_array:
+        if numb > pivot:
+            right.append(numb)
+        else:
+            left.append(numb)
+    return left, pivot, right
+
+
+a = [3,
+     9,
+     8,
+     4,
+     6,
+     10,
+     2,
+     5,
+     7,
+     1]
+for i in [1,2,3]:
+    pivot_settings = i
+    
+    sorted_array, count = sort_and_count_comparisons(a)
+
+    print(sorted_array)
+
+    print(count)
+
+# print(choose_pivot_element(a))
